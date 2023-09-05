f98086c291debd4330475081fb2dba9f0753355e
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-05 16:37:27 -0500

    add method preHandle

--- src/main/java/com/interceptor/ProductServiceInterceptor.java
+++ src/main/java/com/interceptor/ProductServiceInterceptor.java
@@ -1,8 +1,15 @@
 package com.interceptor;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class ProductServiceInterceptor implements HandlerInterceptor {
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    return HandlerInterceptor.super.preHandle(request, response, handler);
+  }
 }
