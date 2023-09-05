fa39131b5c003168062952ad395045d05c910f07
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-05 17:26:39 -0500

    add some flags

--- src/main/java/com/interceptor/ProductServiceInterceptor.java
+++ src/main/java/com/interceptor/ProductServiceInterceptor.java
@@ -11,17 +11,20 @@ public class ProductServiceInterceptor implements HandlerInterceptor {
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    System.out.println("PRE handle method is calling");
     return HandlerInterceptor.super.preHandle(request, response, handler);
   }
 
   @Override
   public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
     HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    System.out.println("Post Handle method is calling");
   }
 
   @Override
   public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
     HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    System.out.println("Request and Responde is completed");
   }
   
 }
