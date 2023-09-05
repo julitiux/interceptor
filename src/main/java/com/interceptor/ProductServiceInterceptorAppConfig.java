dddb5c047f79c290e108aa0ed051327cec4438a2
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-05 16:46:56 -0500

    implement config WebMvcConfigurer

--- src/main/java/com/interceptor/ProductServiceInterceptorAppConfig.java
+++ src/main/java/com/interceptor/ProductServiceInterceptorAppConfig.java
@@ -1,4 +1,19 @@
 package com.interceptor;
 
-public class ProductServiceInterceptorAppConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class ProductServiceInterceptorAppConfig implements WebMvcConfigurer {
+
+  @Autowired
+  ProductServiceInterceptor productServiceInterceptor;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(productServiceInterceptor);
+    WebMvcConfigurer.super.addInterceptors(registry);
+  }
 }
