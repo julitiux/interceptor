5f45ac8cc76aba6ab3d8bcca781b2a57c417d5d9
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-05 16:37:52 -0500

    add method postHandle

--- src/main/java/com/interceptor/ProductServiceInterceptor.java
+++ src/main/java/com/interceptor/ProductServiceInterceptor.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class ProductServiceInterceptor implements HandlerInterceptor {
@@ -11,5 +12,10 @@ public class ProductServiceInterceptor implements HandlerInterceptor {
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
     return HandlerInterceptor.super.preHandle(request, response, handler);
+  }
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
   }
 }
